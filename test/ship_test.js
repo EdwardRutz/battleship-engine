b90ef4d3da7ca29827cd4e4d431c6769684d7212
edwardrutz <96d7cf74@opayq.com>
2018-11-30 19:39:19 -0500

    Test - Test for no ship at players coords

--- test/ship_test.js
+++ test/ship_test.js
@@ -4,7 +4,11 @@ describe('check for ship', function() {
   var checkForShip = require('../game_logic/ship_methods').checkForShip;
 
   it('should correctly report no ship at a given players coordinates', function() {
+    player = {
+      ships: [
 
+      ]
+    }
   });
 
 });
